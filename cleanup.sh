c6f4bb3e5e84d67c05f410f92f2ecd738b73992b
Ritchie Ng <ritchieng@u.nus.edu>
2018-11-13 20:46:47 +0200

    fix: new cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -8,18 +8,23 @@ find . -name "*.pt" -exec rm {} \;
 find . -name "*-ubyte" -exec rm {} \;
 find . -name "*.ipynb" -exec rm {} \;
 find . -name "*.ipynb_checkpoints" -exec rm {} \;
+cd ..
 echo Removed all files
 
+echo copying site for backup
+cp -rf ./site ../site
+
 echo Pushing files to github
-cd ..
 git checkout gh-pages
-cd site
-touch .nojekyll
-cd ..
-git checkout gh-pages
-git add ./site
-git commit -m "Manual deploy"
-git push origin gh-pages --force
+#rm -rf *
+#
+#cp -rf ../site ./
+#touch .nojekyll
+#cd ..
+#git checkout gh-pages
+#git add ./site
+#git commit -m "Manual deploy"
+#git push origin gh-pages --force
 
 echo Checkout master
 git checkout master
