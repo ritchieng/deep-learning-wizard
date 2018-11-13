edbf710fa065732167ad800d86865339722c9a16
Ritchie Ng <ritchieng@u.nus.edu>
2018-11-13 21:00:35 +0200

    fix: final fix for mkdocs deploy

--- cleanup.sh
+++ cleanup.sh
@@ -1,31 +1,6 @@
 #!/usr/bin/env bash
-mkdocs build
-echo Completed building site!
-
-cd site
-echo Cleaning crap files...
+echo Cleaning files for deploy...
 find . -name "*.pt" -exec rm {} \;
 find . -name "*-ubyte" -exec rm {} \;
 find . -name "*.ipynb" -exec rm {} \;
 find . -name "*.ipynb_checkpoints" -exec rm {} \;
-cd ..
-echo Removed all files
-
-echo copying site for backup
-cp -rf ./site ../site
-
-echo Pushing files to github
-git checkout gh-pages
-#rm -rf *
-#
-#cp -rf ../site ./
-#touch .nojekyll
-#cd ..
-#git checkout gh-pages
-#git add ./site
-#git commit -m "Manual deploy"
-#git push origin gh-pages --force
-
-echo Checkout master
-git checkout master
-
