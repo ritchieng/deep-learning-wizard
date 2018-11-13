3b407ea8e97d40ca799f63da4dce773fe3e04eee
Ritchie Ng <ritchieng@u.nus.edu>
2018-11-13 21:07:07 +0200

    fix: simple fix for deploying without heavy files

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,9 @@ find . -name "*.pt" -exec rm {} \;
 find . -name "*-ubyte" -exec rm {} \;
 find . -name "*.ipynb" -exec rm {} \;
 find . -name "*.ipynb_checkpoints" -exec rm {} \;
+
+echo Deploy to pages branch
+mkdocs gh-deploy --force
+
+echo Reset file deletes
+git stash
