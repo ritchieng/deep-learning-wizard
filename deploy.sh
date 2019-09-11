7a4be4d61599ce6f06bde8baa087982e91332515
Ritchie Ng <ritchieng@u.nus.edu>
2019-09-11 11:40:30 +0800

    chore: rm files in bash

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ sudo find . -name "*.pt" -exec rm {} \;
 sudo find . -name "*-ubyte" -exec rm {} \;
 sudo find . -name "*.ipynb" -exec rm {} \;
 sudo find . -name "*.ipynb_checkpoints" -exec rmdir {} \;
+sudo rm -rf ./docs/programming/electron/app-win32-x64/
+sudo rm -rf ./docs/programming/electron/app/node_modules/
 
 echo Deploy to pages branch
 mkdocs gh-deploy --force
