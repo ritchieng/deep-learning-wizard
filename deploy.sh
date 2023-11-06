3db0c4c8da8051acab167ef81d44e45b2828c991
Ritchie Ng <ritchieng@u.nus.edu>
2023-11-06 11:16:50 +0800

    Fix bash deploy

--- deploy.sh
+++ deploy.sh
@@ -10,19 +10,12 @@ wait
 echo Cleaning files for deployment...
 cd dlw-deploy
 sudo find . -name "*.simg" -exec rm {} \;
-echo Done S1
 sudo find . -name "*.pt" -exec rm {} \;
-echo Done S2
 sudo find . -name "*-ubyte" -exec rm {} \;
-echo Done S3
 sudo find . -name "*.ipynb" -exec rm {} \;
-echo Done S4
 sudo find . -name "*.ipynb_checkpoints" -exec rmdir {} \;
-echo Done S5
 sudo rm -rf ./docs/programming/electron/app-win32-x64/
-echo Done S6
 sudo rm -rf ./docs/programming/electron/app/node_modules/
-echo Done S7
 wait
 
 echo Deploying to pages branch...
