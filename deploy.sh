894cc40ea694a6e708d8c817886b91e2066ed611
ritchieng <ritchieng@u.nus.edu>
2024-07-24 11:22:12 +0800

    Update bash deploy script verbosity

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,17 @@ wait
 echo Cleaning files for deployment...
 cd dlw-deploy
 sudo find . -name "*.simg" -exec rm {} \;
+echo Cleaned *.simg
 sudo find . -name "*.pt" -exec rm {} \;
+echo Cleaned *.pt
 sudo find . -name "*-ubyte" -exec rm {} \;
+echo Cleaned *.-ubyte
 sudo find . -name "*.ipynb" -exec rm {} \;
 sudo find . -name "*.ipynb_checkpoints" -exec rmdir {} \;
+echo Cleaned *.ipynb
 sudo rm -rf ./docs/programming/electron/app-win32-x64/
 sudo rm -rf ./docs/programming/electron/app/node_modules/
+echo Cleaned electron
 wait
 
 echo Deploying to pages branch...
